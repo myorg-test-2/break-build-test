677c7170efe232496912ef588089e4e1c702973c
allenl <a9115077@gapps.ntust.edu.tw>
2026-03-30 08:40:19 +0800

    Switch to Python for buildless capture support

---
+++ app.py
@@ -0,0 +1,29 @@
+import os
+import sqlite3
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/user')
+def get_user():
+    user_id = request.args.get('id')
+    conn = sqlite3.connect('test.db')
+    # SQL Injection
+    result = conn.execute("SELECT * FROM users WHERE id = " + user_id)
+    return str(result.fetchall())
+
+@app.route('/ping')
+def ping():
+    host = request.args.get('host')
+    # Command Injection
+    output = os.popen("ping -c 4 " + host).read()
+    return output
+
+@app.route('/hello')
+def hello():
+    name = request.args.get('name')
+    # XSS
+    return "<h1>Hello " + name + "</h1>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
